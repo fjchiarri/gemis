30121669e5bea918a3e7ce8530b7ec71adb9e37e
fjchiarri <francisco-javier.chiarri@sopra.com>
2018-03-14 18:14:28 +0100

    sample list - prime input

--- src/app/sample/list/list.component.ts
+++ src/app/sample/list/list.component.ts
@@ -20,17 +20,10 @@ export class ListComponent implements OnInit, OnDestroy {
   // The displayed data set (potentially filtered)
   rowData: User[];
 
+  filterValue: string;
+
   // Column declarations
   public columns:Array<any> = [
-    { title: 'id', name: 'id', filtering: {filterString: '', placeholder: 'Filter by id'}},
-    { title: 'name', name: 'name', filtering: {filterString: '', placeholder: 'Filter by name'}},
-    { title: 'e-mail', name: 'email', sort: 'asc'},
-    { title: 'phone', name: 'phone', sort: '', filtering: {filterString: '', placeholder: 'Filter by phone'}},
-    { title: 'user', name: 'username'},
-    { title: 'website', name: 'website'}
-  ];
-
-  public columns_prime:Array<any> = [
     { field: 'id', header: 'id' },
     { field: 'name', header: 'name' },
     { field: 'e-mail', header: 'email' },
@@ -39,21 +32,6 @@ export class ListComponent implements OnInit, OnDestroy {
     { field: 'website', header: 'website' }
   ];
 
-  // Table config variables
-  public page:number = 1;
-  public itemsPerPage:number = 5;
-  public maxSize:number = 5;
-  public numPages:number = 1;
-  public length:number = 0;
-
-  // Table config
-  public config:any = {
-    paging: true,
-    sorting: {columns: this.columns},
-    filtering: {filterString: ''},
-    className: ['table-striped', 'table-bordered']
-  };
-
   // Constructor
   constructor(private sampleService: SampleService) {}
 
@@ -61,8 +39,6 @@ export class ListComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.sub = this.sampleService.usersChanged.subscribe(users => {
       this.fullData = users as User[];
-      this.length = this.fullData.length;
-      this.onChangeTable(this.config);
     });
   }
 
@@ -71,106 +47,7 @@ export class ListComponent implements OnInit, OnDestroy {
     this.sub.unsubscribe();
   }
 
+  onFilterTable() {
 
-
-  // Event triggered on cell click
-  public onCellClick(data: any): any {
-    console.log(data);
-  }
-
-  public onCellDblClick(data: any): any {
-    console.log(2);
-  }
-
-  // Function called on page change
-  public changePage(page:any, data:Array<any> = this.fullData):Array<any> {
-    let start = (page.page - 1) * page.itemsPerPage;
-    let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
-    return data.slice(start, end);
-  }
-
-  // Function called on sorting change
-  public changeSort(data:any, config:any):any {
-    if (!config.sorting) {
-      return data;
-    }
-
-    let columns = this.config.sorting.columns || [];
-    let columnName:string = void 0;
-    let sort:string = void 0;
-
-    for (let i = 0; i < columns.length; i++) {
-      if (columns[i].sort !== '' && columns[i].sort !== false) {
-        columnName = columns[i].name;
-        sort = columns[i].sort;
-      }
-    }
-
-    if (!columnName) {
-      return data;
-    }
-
-    // simple sorting
-    return data.sort((previous:any, current:any) => {
-      if (previous[columnName] > current[columnName]) {
-        return sort === 'desc' ? -1 : 1;
-      } else if (previous[columnName] < current[columnName]) {
-        return sort === 'asc' ? -1 : 1;
-      }
-      return 0;
-    });
-  }
-
-  // Function called on filter change
-  public changeFilter(data:any, config:any):any {
-    let filteredData:Array<any> = data;
-    this.columns.forEach((column:any) => {
-      if (column.filtering) {
-        filteredData = filteredData.filter((item:any) => {
-          return item[column.name].toString().match(column.filtering.filterString);
-        });
-      }
-    });
-
-    if (!config.filtering) {
-      return filteredData;
-    }
-
-    if (config.filtering.columnName) {
-      return filteredData.filter((item:any) =>
-        item[config.filtering.columnName].toString().match(this.config.filtering.filterString));
-    }
-
-    let tempArray:Array<any> = [];
-    filteredData.forEach((item:any) => {
-      let flag = false;
-      this.columns.forEach((column:any) => {
-        if (item[column.name].toString().toString().match(this.config.filtering.filterString)) {
-          flag = true;
-        }
-      });
-      if (flag) {
-        tempArray.push(item);
-      }
-    });
-    filteredData = tempArray;
-
-    return filteredData;
-  }
-
-  // Function called on table changes (data, filters, or sorting)
-  public onChangeTable(config:any, page:any = {page: this.page, itemsPerPage: this.itemsPerPage}):any {
-    if (config.filtering) {
-      Object.assign(this.config.filtering, config.filtering);
-    }
-
-    if (config.sorting) {
-      Object.assign(this.config.sorting, config.sorting);
-    }
-
-    let filteredData = this.changeFilter(this.fullData, this.config);
-    let sortedData = this.changeSort(filteredData, this.config);
-    this.rowData = page && config.paging ? this.changePage(page, sortedData) : sortedData;
-    this.length = sortedData.length;
   }
 }
--- src/app/sample/sample.module.ts
+++ src/app/sample/sample.module.ts
@@ -7,10 +7,9 @@ import { DisplayComponent } from './display/display.component';
 import { EditComponent } from './edit/edit.component';
 import { SampleService } from './sample.service';
 import { HttpClientModule } from '@angular/common/http';
-import { Ng2TableModule } from 'ng2-table/ng2-table';
 import { FormsModule } from '@angular/forms';
-import { PaginationModule } from 'ng2-bootstrap/pagination';
 import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
 
 
 @NgModule({
@@ -23,10 +22,9 @@ import { TableModule } from 'primeng/table';
   imports: [
     BrowserModule,
     HttpClientModule,
-    Ng2TableModule,
-    PaginationModule.forRoot(),
     FormsModule,
-    TableModule
+    TableModule,
+    InputTextModule
   ],
   exports: [
     SampleComponent,
